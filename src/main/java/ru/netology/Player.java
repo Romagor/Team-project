72b5e909f155a5a9762bf097b03a1563b8ea8389
RomaGor <goryachevskii60@gmail.com>
2022-08-18 12:15:07 +0300

    fixed #6

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -39,9 +39,9 @@ public class Player {
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game) + hours);
+            playedTime.put(game, playedTime.getOrDefault(game, hours) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("Игра " + game.getTitle() + " не установлена");
         }
         return playedTime.get(game);
     }

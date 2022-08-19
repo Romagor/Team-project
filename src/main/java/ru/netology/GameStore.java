b6ed158d50f54f4208cfc2d5fc0d04a9123fd7ee
Marina1977D <deevamariha@gmail.com>
2022-08-19 08:32:30 +0300

    closed #5

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -73,6 +73,11 @@ public class GameStore {
      * за играми этого каталога
      */
     public int getSumPlayedTime() {
-        return 0;
+        int sum = 0;
+        ArrayList<Integer> playedHours = new ArrayList<>(playedTime.values());
+        for (Integer hours : playedHours) {
+            sum +=hours;
+        }
+        return sum;
     }
 }
